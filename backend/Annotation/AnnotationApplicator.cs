3f7507dfe71be7b6b6a02d3f1624a5cafeaf550b
void0xf <150953141+void0xf@users.noreply.github.com>
2026-04-30 14:37:25 +0200

    Add Document Annotation formatting components

---
+++ backend/Annotation/AnnotationApplicator.cs
@@ -0,0 +1,46 @@
+using backend.Application.Validation;
+using DocumentFormat.OpenXml.Packaging;
+using ThesisValidator.Rules;
+
+namespace backend.Annotation;
+
+public sealed class AnnotationApplicator
+{
+    public void Apply(
+        WordprocessingDocument document,
+        IEnumerable<RuleExecution> executions)
+    {
+        var commentService = new CommentWriter();
+
+        foreach (var execution in executions)
+        {
+            AddCommentForProblem(
+                commentService,
+                document,
+                execution.Problem);
+        }
+    }
+
+    private static void AddCommentForProblem(
+        CommentWriter commentService,
+        WordprocessingDocument document,
+        RuleProblem problem)
+    {
+        switch (problem.AnnotationTarget)
+        {
+            case ParagraphAnnotationTarget paragraphTarget:
+                commentService.AddCommentToParagraph(
+                    document,
+                    paragraphTarget.Paragraph,
+                    problem.Message);
+                break;
+
+            case RunAnnotationTarget runTarget:
+                commentService.AddCommentToRun(
+                    document,
+                    runTarget.Run,
+                    problem.Message);
+                break;
+        }
+    }
+}
